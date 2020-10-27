8b1192350ad4e5f1f3b90c38e60b0937c07f67d6
Grim <silencegrim@gmail.com>
2020-10-27 18:36:00 -0400

    init

---
+++ windows/CorvusCollective/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::CorvusCollective::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::CorvusCollective::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
